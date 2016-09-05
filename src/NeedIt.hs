55cf4e35b2aef7bd75586e15213876172a730f55
Marcelo Camargo <marcelocamargo@linuxmail.org>
2016-09-05 14:46:02 -0300

    Generic and basic dependency parser

--- src/NeedIt.hs
+++ src/NeedIt.hs
@@ -1,21 +1,47 @@
 {-# LANGUAGE ScopedTypeVariables #-}
-module NeedIt (readDeps, extractSource, clearEmptyLines) where
+module NeedIt (readDeps, extractSource, validCommands, prefix, collect') where
 import System.IO
-import Control.Exception
+import Control.Exception (handle, IOException)
+import Data.List (partition)
+import Data.Char (isSpace)
 
 data Source = Source { package :: [String]
-                     , deps :: [String] }
+                     , deps :: [String]
+                     }
                      deriving Show
 
 readDeps :: IO (Maybe [String])
 readDeps = handle (\(e :: IOException) -> return Nothing)
     ((openFile "DEPENDENCIES" ReadMode) >>= hGetContents >>= return . Just . lines)
 
-clearEmptyLines :: [String] -> [String]
-clearEmptyLines = filter (not . null)
+validCommands :: [String]
+validCommands = ["PACKAGE", "NEEDS", ""]
 
-extractSource :: IO ()
-extractSource = readDeps >>= \l -> case l of
-    Just xs -> mapM_ (print) $ clearEmptyLines xs
-    Nothing -> error "eita"
+prefix :: String -> String
+prefix = takeWhile (not . isSpace)
+
+invalidLine :: [String] -> Int
+invalidLine = invalidLine' 1
+
+invalidLine' :: Int -> [String] -> Int
+invalidLine' n [] = -1
+invalidLine' n (x:xs) | not $ elem (prefix x) validCommands = n
+                      | otherwise                           = invalidLine' (n + 1) xs
+
+getValue :: [String] -> [String]
+getValue = map (tail . dropWhile (not . isSpace))
+
+collect' :: [String] -> ([String], [String])
+collect' xss = let (packages, rest) = partition (\x -> (prefix x) == "PACKAGE") xss
+    in (getValue packages, getValue $ filter (\x -> (prefix x) == "NEEDS") rest)
+
+extractSource :: IO (Either String Source)
+extractSource = readDeps >>= \monad -> return $ case monad of
+    Nothing -> Left "Failed to open DEPENDENCIES"
+    Just xs -> case invalidLine xs of
+        -1 -> let (package, deps) = collect' xs in case length package of
+            0 -> Left "Package name not informed"
+            1 -> Right $ Source { package = package, deps = deps }
+            _ -> Left "More than one entry for package name"
+        n  -> Left $ "Parsing error on line " ++ (show n) ++ ": " ++ (xs !! (n - 1))
 
